176b8918c4bad785cbdce0d81460db63ef9c2136
Alexander Lapena <alapena@mail.uoguelph.ca>
2018-12-16 22:09:39 -0500

    Allow adding notes

--- app/src/main/java/com/example/alex/rap_homie/NoteSelect.java
+++ app/src/main/java/com/example/alex/rap_homie/NoteSelect.java
@@ -1,5 +1,6 @@
 package com.example.alex.rap_homie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -36,8 +37,8 @@ public class NoteSelect extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent myIntent = new Intent( NoteSelect.this, NotepadActivity.class);
+                NoteSelect.this.startActivity(myIntent);
             }
         });
 
