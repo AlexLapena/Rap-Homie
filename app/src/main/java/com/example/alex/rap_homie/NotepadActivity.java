ce35526038ff7e988251e2fc167f5d16894f18b1
Alexander Lapena <alapena@mail.uoguelph.ca>
2019-03-17 22:22:01 -0400

    Add popup window

--- app/src/main/java/com/example/alex/rap_homie/NotepadActivity.java
+++ app/src/main/java/com/example/alex/rap_homie/NotepadActivity.java
@@ -5,10 +5,16 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -44,8 +50,43 @@ public class NotepadActivity extends AppCompatActivity {
             }
         });
 
+        final Button button = findViewById(R.id.rhyme_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onButtonShowPopupWindowClick(v);
+            }
+        });
+
         songText = (EditText) findViewById(R.id.EditText1);
         songText.setText(Open(titleText.getText().toString()));
+    }
+
+    // https://stackoverflow.com/questions/5944987/how-to-create-a-popup-window-popupwindow-in-android
+    public void onButtonShowPopupWindowClick(View view) {
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_window, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 
     @Override
