d96e8d8b1abf158cdbc8a41e50dbea24f35a4a2a
Alexander Lapena <alapena@mail.uoguelph.ca>
2019-03-17 22:48:23 -0400

    Update button string

--- app/src/main/java/com/example/alex/rap_homie/NotepadActivity.java
+++ app/src/main/java/com/example/alex/rap_homie/NotepadActivity.java
@@ -90,7 +90,7 @@ public class NotepadActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean  onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
